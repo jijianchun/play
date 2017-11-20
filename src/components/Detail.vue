1f0be6e447ae361e11f3314fa2d1ec82a1e0ed86
jijianchun <549332732@qq.com>
2017-11-20 16:37:11 +0800

    完善详情页

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -5,44 +5,48 @@
 			<dd>{{player.player_name}}</dd>
 		</dl>
 		<dl>
+			<dt>球员头像</dt>
+			<dd><img :src="player.player_image"></dd>
+		</dl>
+		<dl>
 			<dt>球员英文全称</dt>
-			<dd></dd>
+			<dd>{{player.full_name}}</dd>
 		</dl>
 		<dl>
 			<dt>球员身高</dt>
-			<dd></dd>
+			<dd>{{player.player_height}}</dd>
 		</dl>
 		<dl>
 			<dt>球员体重</dt>
-			<dd></dd>
+			<dd>{{player.player_weight}}</dd>
 		</dl>
 		<dl>
 			<dt>球员位置</dt>
-			<dd></dd>
+			<dd>{{player.player_place}}</dd>
 		</dl>
 		<dl>
 			<dt>球员生日</dt>
-			<dd></dd>
+			<dd>{{player.player_brithday}}</dd>
 		</dl>
 		<dl>
 			<dt>出生城市</dt>
-			<dd></dd>
+			<dd>{{player.player_city}}</dd>
 		</dl>
 		<dl>
 			<dt>毕业学校</dt>
-			<dd></dd>
+			<dd>{{player.player_college}}</dd>
 		</dl>
 		<dl>
 			<dt>球衣号码</dt>
-			<dd></dd>
+			<dd>{{player.player_number}}</dd>
 		</dl>
 		<dl>
 			<dt>选秀情况</dt>
-			<dd></dd>
+			<dd>{{player.player_draft}}</dd>
 		</dl>
 		<dl>
 			<dt>当前年薪</dt>
-			<dd></dd>
+			<dd>{{player.player_salary}}</dd>
 		</dl>
 
         
@@ -79,13 +83,29 @@
 
 
 <style lang="less" scoped>
+	@import '../style/common';
 	@import '../style/mixin';
 	.box{
+		padding:10px;
 		dl{
 			display:flex;
 			flex-direction:row;
+			height:80/@r;
+			line-height:80/@r;
+			padding:5px;
+			border-bottom:1px dashed #e8e8e8;
 			dt{
-				
+				width:35%;
+				font-size:25/@r;
+				text-align:center;
+			}
+			dd{
+				flex:1;
+				font-size:25/@r;
+				text-align:left;
+				img{
+					height:100%;
+				}
 			}
 		}
 	}
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,7 +10,6 @@
                 </li>
             </ul>
         </el-row>
-        <canvas id="canvas" ref="mybox" width="200" height="200"></canvas>
 		
   	</div>
 </template>
@@ -27,9 +26,6 @@
                 players: []
     		}
     	},
-        mounted(){
-            this.initdata();
-        },
         methods:{
             // 监听子组件的事件并获取数据
             showMsgFromChild:function(data){
@@ -42,55 +38,6 @@
             // 去详情页面
             goDetail:function(player_id){
                 this.$router.push({path: '/Detail?id=' + player_id})
-            },
-            initdata:function(){
-                let canvas = document.getElementById("canvas");
-                console.log(canvas);
-                let ctx = canvas.getContext('2d');
-                let radians = (Math.PI / 180) * 180;
-                let startTime = Date.now();
-                let time = 2000;
-                let clockwise = 1;
-                let cp1x, cp1y, cp2x, cp2y;
-                
-                // 初始状态
-                // ctx.bezierCurveTo(90, 28, 92, 179, 200, 100);
-                // 末尾状态
-                // ctx.bezierCurveTo(145, 100, 41, 100, 200, 100);
-                
-                requestAnimationFrame(function waveDraw() {  
-                    let t = Math.min(1.0, (Date.now() - startTime) / time);
-                      
-                    if(clockwise) {
-                        cp1x = 90 + (55 * t);
-                        cp1y = 28 + (72 * t);
-                        cp2x = 92 - (51 * t);
-                        cp2y = 179 - (79 * t);
-                    } else {
-                        cp1x = 145 - (55 * t);
-                        cp1y = 100 - (72 * t);
-                        cp2x = 41 + (51 * t);
-                        cp2y = 100 + (79 * t);
-                    }
-                    
-                    ctx.clearRect(0, 0, 200, 200); 
-                    ctx.beginPath();
-                    ctx.moveTo(0, 100);
-                    // 绘制三次贝塞尔曲线
-                    ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, 200, 100);
-                    // 绘制圆弧
-                    ctx.arc(100, 100, 100, 0, radians, 0);
-                    ctx.fillStyle = "rgba(154, 205, 50, .8)";
-                    ctx.fill();
-                    ctx.save();  
-                    
-                    if( t == 1 ) {
-                        startTime = Date.now();
-                        clockwise = !clockwise;
-                    } 
-
-                    requestAnimationFrame(waveDraw);
-                });
             }
         },
     	components:{
@@ -135,16 +82,5 @@
                 }
             }
         }
-    }
-    #canvas {
-        margin-top:100px;
-        box-sizing: content-box;
-        padding: 5px;
-        background-color: #fff;
-        border-radius: 100%;
-        position: relative;
-        top: 50%;
-        transform: translateY(-50%);
-        border: 10px solid rgba(154, 205, 50, .8);  
     }
 </style>
