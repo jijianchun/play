838ac00073ccca1646cfcbdc48146d7f2f6b472c
健男-春 <549332732@qq.com>
2017-11-23 15:55:08 +0800

    add head2

---
+++ src/components/head2Top.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="header_container">
+
+		<div class="back" @click="back">
+			<i class="el-icon-arrow-left"></i>
+		</div>
+		<div class="search-box">
+			球员详情
+		</div>
+		<div class="space">
+			
+		</div>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+	data() {
+		return {
+			
+		}
+	},
+	methods: {
+		back(){
+			this.$router.go(-1);
+		}
+	}
+}
+    
+</script>
+
+<style lang="less" scoped>
+	@import '../style/mixin';
+	.header_container{
+		background-color: #EFF2F7;
+		height: 60px;
+		display: flex;
+		align-items: center;
+		position:fixed;
+		top:0;
+		left:0;
+		width:100%;
+		.back{
+			width:70/@r;
+		}
+		.search-box{
+			flex:1;
+			font-size:30/@r;
+		}
+		.space{
+			width:60/@r;
+		}
+	}
+</style>
